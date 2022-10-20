bf7a85ae9e04fb34667b28060408e9f3af4f230e
Junichi Kato <j5ik2o@gmail.com>
2022-10-20 22:45:17 +0900

    add impls.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module desgin-patterns-in-golang
 
 go 1.19
+
+require github.com/igrmk/treemap/v2 v2.0.1
+
+require golang.org/x/exp v0.0.0-20220317015231-48e79f11773a // indirect
---
+++ mediator/impl_test.go
@@ -0,0 +1,18 @@
+package mediator
+
+import (
+	"testing"
+)
+
+func TestMediator(t *testing.T) {
+
+	mediator := NewConcreteMediator()
+	colleagueA := NewConcreteColleagueA(mediator, "A")
+	colleagueB := NewConcreteColleagueB(mediator, "B")
+
+	mediator.AddColleague(colleagueA)
+	mediator.AddColleague(colleagueB)
+
+	colleagueA.Run()
+	colleagueB.Run()
+}
